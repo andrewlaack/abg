6818c52f88bd8e80dc2e1e4e9381d42d86b901a7
Andrew Laack <andrew@laack.co>
2026-09-16 17:28:03 -0500

    Some benchmarking

--- benchmarking/bench.sh
+++ benchmarking/bench.sh
@@ -1,5 +1,5 @@
 while [ 1 ]; do
     /usr/bin/time -o out -f '%S,%U,%e' ./abg.out -s 0 --edges 10000 --vertices 1000 >/dev/null 2>&1
-    cat out | tee -a benchmarking/fg_render_v_1000_e_10000_s_0/out.csv
+    cat out | tee -a benchmarking/fg_render_v_1000_e_10000_s_0/out1.csv
     sleep 10
 done
